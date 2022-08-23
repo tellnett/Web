3c8924915807b6c861e341c5e17bf6814ef7330b
Wellington Nascimento <37088731+tellnett@users.noreply.github.com>
2022-08-23 16:14:58 -0300

    Update funcao.js

--- ProjetoMoto/javascript/funcao.js
+++ ProjetoMoto/javascript/funcao.js
@@ -1,7 +1,7 @@
 let texto1btn = document.getElementById("red");
 let texto2btn = document.getElementById("blue");
 let texto3btn = document.getElementById("black");
-let foto1 = document.getElementById("fot");
+
 
 texto1btn.onclick = function () {
     moto.style.backgroundImage = "url(img/BMW1.png)"
@@ -23,8 +23,4 @@ texto3btn.onclick = function () {
     document.getElementById("h2").style.color = "black";
     document.getElementById("escolha1").innerHTML = "Cor escolhida é black";
     document.getElementById("textomodelo").innerHTML = "Uma boa imagem é característica pertencente dessa linhagem e a G 310 R traz alguns fortes traços da S 1000 R, irmã mais velha da linha Roadster da BMW. A carenagem esportiva do farol, o friso dinâmico do tanque de combustível e as dimensões típicas de uma naked não deixam este modelo passar despercebido nas ruas.";
-}
-foto1.onclick = function () {
-alert("bem vindo ao curso de web com javascript");
-    
 }
