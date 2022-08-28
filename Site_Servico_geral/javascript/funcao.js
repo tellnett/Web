29272a651415ee3b424a1a324252ee2dc1755b83
Wellington Nascimento <37088731+tellnett@users.noreply.github.com>
2022-08-28 16:28:54 -0300

    Add files via upload

--- Site_Servico_geral/javascript/funcao.js
+++ Site_Servico_geral/javascript/funcao.js
@@ -3,8 +3,8 @@ window.addEventListener('scroll',()=>{
     ('window-scroll',window.scrollY > 0)
 
 })
+/*========= INICIO da API exibir e ocultar faq de perguntas======== */
 
-// exibir e ocultar faq de perguntas
 const faqs = document.querySelectorAll('.faq');
 faqs.forEach(faq =>{
     faq.addEventListener('click',() =>{
@@ -36,4 +36,14 @@ var swiper = new Swiper(".mySwiper", {
     }
   });
 
-/*========= FIM da API do Depoimento dos clientes======== */
+/*========= INICIO DA API do Menu ======== */
+var ul =document.querySelector('nav ul');
+var menuBtn = document.querySelector('.menu-btn i');
+
+function menuShow(){
+    if(ul.classList.contains('open')){
+        ul.classList.remove('open');
+     } else{
+        ul.classList.add('open');
+        }
+    }
